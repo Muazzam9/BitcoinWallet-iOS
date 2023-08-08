aa8a7507ca3c1c5e72bdc3161d3a487f3a634a5c
Muazzam <muazzam.aziz@gmail.com>
2023-08-08 17:44:58 +0200

    Initial Commit

---
+++ BitcoinWallet-iOS/BitcoinWallet_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinWallet_iOSApp.swift
+//  BitcoinWallet-iOS
+//
+//  Created by Muazzam.Aziz on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinWallet_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
